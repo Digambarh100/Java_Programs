3a7fb63af8b9cc69662fed2609f1c65ab0a28926
Digambar Hawaldar <109816894+Digambarh100@users.noreply.github.com>
2024-06-18 23:32:59 +0530

    Update DupliatElementinArray.java

--- src/com/array/DupliatElementinArray.java
+++ src/com/array/DupliatElementinArray.java
@@ -1,6 +1,6 @@
 package com.array;
 
-public class DupliatElementinArray {
+public class DupliatElementinArra {
 
 	public static void main(String[] args) {
 	int [] arr = new int [] {1, 2, 3, 4, 2, 7, 8, 8, 3};   
