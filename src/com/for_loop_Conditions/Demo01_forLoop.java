c1d8727eb59568658ab3a74a81239da3017442ed
unknown <digambarh100@gmail.com>
2024-06-21 15:29:29 +0530

    Code changes1

--- src/com/for_loop_Conditions/Demo01_forLoop.java
+++ src/com/for_loop_Conditions/Demo01_forLoop.java
@@ -1,7 +1,7 @@
 package com.for_loop_Conditions;
 
 public class Demo01_forLoop {
-
+// 10 to 1
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		for (int i = 10; i>0;i--) {
