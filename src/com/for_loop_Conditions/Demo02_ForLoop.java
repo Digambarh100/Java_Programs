f7b81d9a98680e7cd8899b60bb4124ae474bde08
unknown <digambarh100@gmail.com>
2024-06-21 15:31:00 +0530

    Code changes 2

--- src/com/for_loop_Conditions/Demo02_ForLoop.java
+++ src/com/for_loop_Conditions/Demo02_ForLoop.java
@@ -5,15 +5,14 @@ public class Demo02_ForLoop {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		for (int i = 1; i <= 10; i++) {
-			for (int j = 1; j <= 5; j++) {
-				System.out.printf("i=" + i + " J=" + j + " ");
+		for(int i=1;i<=10;i++) {
+			for(int j=1;j<=5;j++) {
+				System.out.print("i="+i+" J="+j+" ");
 				//break;
 			}
 			System.out.println();
-			//break;
+		//	break;
 		}
-
 	}
 
 }
