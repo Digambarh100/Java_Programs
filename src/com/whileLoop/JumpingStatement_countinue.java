fdffeeb122c52d1061799738054e16fcae9f51b1
unknown <digambarh100@gmail.com>
2024-06-21 15:39:39 +0530

    code changes

---
+++ src/com/whileLoop/JumpingStatement_countinue.java
@@ -0,0 +1,14 @@
+package com.whileLoop;
+
+public class JumpingStatement_countinue {
+
+	public static void main(String[] args) {
+
+	
+		for (int i = 0; i < 10; i++) {
+			if (i == 5)
+				break;
+			System.out.println(i);
+		}
+	}
+}
---
+++ src/com/whileLoop/No_1_10_10_1.java
@@ -0,0 +1,25 @@
+package com.whileLoop;
+
+public class No_1_10_10_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+// i to 10 
+//		int i =1;
+//		while(i<=10) {
+//			System.out.println(i);
+//			i++;
+//		}
+		
+		
+//		10 to 1 
+		int i = 10;
+		while(i>0) {
+			System.out.println(i);
+			i--;
+		}	
+		
+		
+	}
+
+}
